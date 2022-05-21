ee2c1b988cdb09ccb0c606bd4aabf76e8fd81c72
Kenneth Dow <kdow@epages.com>
2022-05-21 12:43:20 +0200

    Add submodule
    Add ffmpeg container
    
    Add mongodb
    
    Working nginx and php containers with depencies installed
    
    Add env file and gitignore
    
    Initial Commit

---
+++ prep.sh
@@ -0,0 +1,11 @@
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+
+echo "building php Image"
+docker compose build
+echo "starting docker"
+docker compose up -d
+
+echo "remove default.conf file"
+docker exec -d nginx rm /etc/nginx/conf.d/default.conf
+docker exec -d nginx service nginx reload
